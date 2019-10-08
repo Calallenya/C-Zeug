c29eae0eaeae7d0460c8f2609594983a19111669
Calallenya <55451314+Calallenya@users.noreply.github.com>
2019-10-08 15:29:41 +0200

    Update Temperatur - Umwandler.c

--- Temperatur - Umwandler.c
+++ Temperatur - Umwandler.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 int main()
 {
+    float a = 3;
+    a  = a * 0.5;
+    printf("%f", a);
     //Variablen Deklaration
     float tempIn, tempOut;
-    int unitIn, unitOut;
-    int desiredConversion;
+    int unitIn, unitOut,i = 0, desiredConversion;
+    float skala_fuer_temperaturanzeige;
     char response;
+    char gradc_beschriftung[] = "0                                             100C";
+    char gradf_beschriftung[] = "32                                            212F";
+    char kelvin_beschriftung[]= "273                                           373K";
+    char romer_beschriftung[] = "7.5                                           60Ro";
     printf("\n\nProgramm zur Umrechnung von Temperaturen\n");
     printf("----------------------------------------\n");
 
@@ -16,6 +23,10 @@ int main()
         printf("1: grad C     2: grad F     3: K    4: Ro \n\n");
         printf("Ihre Wahl: ");
         scanf("%i",&unitIn);    // Input durch user
+        if (unitIn < 1 || unitIn > 4){
+            printf("Falsche Eingabe!");
+            main();
+        }
         while (getchar() != '\n');  // Input Buffer reset
 
         printf("\nBitte geben Sie die umzurechnende Temperatur an(nicht unter dem absoluten Nullpunkt): ");
@@ -73,8 +84,57 @@ int main()
                 printf("\n\n%.2fRo = %.2fK\n\n",tempIn,tempOut);
                 break;
         }
+        printf("--------------------------------------------------\n");
+        if (unitOut == 1){
+            skala_fuer_temperaturanzeige = 100 - tempOut;
+            if (skala_fuer_temperaturanzeige <= 0){printf("\n");}
+            else if (skala_fuer_temperaturanzeige >= 100){while (i < 50) {printf("*");i++;}}
+            else {while (i < tempOut / 2){printf("*");i++;}}
+            printf("\n--------------------------------------------------\n");
+            i = 0;
+            for(i;i < sizeof(gradc_beschriftung);i++) {
+                printf("%c", gradc_beschriftung[i]);
+
+
+            }} else if (unitOut == 2){
+            skala_fuer_temperaturanzeige = tempOut - 32; //32 212
+            if(skala_fuer_temperaturanzeige <= 0){printf("\n");}
+            else if (skala_fuer_temperaturanzeige >= 180){while (i < 50) {printf("*");i++;}}
+            else {while (i < (skala_fuer_temperaturanzeige / 180) * 50){printf("*");i++;}}        //180, da 212 - 32=180
+            printf("\n--------------------------------------------------\n");
+            i = 0;
+            for(i;i < sizeof(gradf_beschriftung);i++) {
+                printf("%c", gradf_beschriftung[i]);
+
+
+            }} else if (unitOut == 3){
+            skala_fuer_temperaturanzeige = tempOut - 273;       //273       373
+            if(skala_fuer_temperaturanzeige <= 0){printf("\n");}
+            else if (skala_fuer_temperaturanzeige >= 100){while (i < 50) {printf("*");i++;}}
+            else {while(i < (skala_fuer_temperaturanzeige / 100) * 50){printf("*");i++;}}
+            printf("\n--------------------------------------------------\n");
+            i = 0;
+            for(i;i < sizeof(kelvin_beschriftung);i++) {
+                printf("%c", kelvin_beschriftung[i]);
+
+
+            }} else if (unitOut == 4){
+            skala_fuer_temperaturanzeige = tempOut - 7.5;       //7.5    60Ro
+            if(skala_fuer_temperaturanzeige <= 0){printf("\n");}
+            else if (skala_fuer_temperaturanzeige >= 100){while (i < 50) {printf("*");i++;}}
+            else {while(i < (skala_fuer_temperaturanzeige / 52.5) * 50){printf("*");i++;}}
+            printf("\n--------------------------------------------------\n");
+            i = 0;
+            for(i;i < sizeof(romer_beschriftung);i++) {
+                printf("%c", romer_beschriftung[i]);
+            }} else {
+            printf("Falsche Eingabe!");
+            main();
+        }
+
         printf("\n\nerneute Berechnung? (j/n)");
         scanf("%c",&response);  // Input durch user
         while (getchar() != '\n');  // Input Buffer reset
+        i = 0;  //i neu initialisieren, da bei Begin der do Schleife i noch den alten Wert hat
     } while(response == 'j' || response == 'J');    //Bedingung um 'do Schleife' zu wiederholen
 }
