4d3c6de07be94997605b34686f67150a313280c7
Calallenya <55451314+Calallenya@users.noreply.github.com>
2019-10-23 16:24:42 +0200

    Update Kalender.c

--- Kalender.c
+++ Kalender.c
@@ -3,9 +3,9 @@
 #include <math.h>
 int day_of_the_year(int tag, int monat, bool sjahr);
 const char *new_year_day(double tag, int monat, double jahr, double last_two_digits_of_year, double first_two_digits_of_year);
-
+int kalenderwoche(double tag, int monat, int year, bool sjahr, double i);
 int main(){
-    int tag_nummber, day, month, year, last_two_digits_of_year = 0, first_two_digits_of_year = 0;
+    int tag_nummber, day, month, year, last_two_digits_of_year = 0, first_two_digits_of_year = 0, i = 1;
     bool schaltjahr = false;
     printf("Datum eingeben [Tag] [Monat] [Jahr]\n");
     scanf("%d %d %d", &day, &month, &year);
@@ -32,7 +32,7 @@ int main(){
         }
 
 
-    //Februar
+        //Februar
     } else if(month == 2){
         if(day > 29 && schaltjahr == true){
             printf("Tag: %d ist nicht richtig, da %d ein Schaltjahr ist\n", day, year);
@@ -43,7 +43,7 @@ int main(){
         }
 
 
-    //Monate mit 31 Tagen
+        //Monate mit 31 Tagen
     } else{
         if(day > 31){
             printf("Falsche Eingabe: Monat %d hat keine %d Tage", month, day);
@@ -52,9 +52,10 @@ int main(){
     }
 
 
-    printf("\nEs ist der %d. Tag des Jahres %d\n", year, day_of_the_year(day, month, schaltjahr));
+    printf("\nEs ist der %d. Tag des Jahres %d\n", day_of_the_year(day, month, schaltjahr), year);
     printf("Der eingegebene Tag ist ein: %s\n", new_year_day(day, month, year, last_two_digits_of_year = 0, first_two_digits_of_year = 0));
     printf("Der 1.Januar des Jahres %d ist ein: %s\n", year, new_year_day(1, 1, year, last_two_digits_of_year = 0, first_two_digits_of_year = 0));
+    kalenderwoche(day_of_the_year(day, month, schaltjahr), month, year, schaltjahr, i);
     return 0;
 }
 
@@ -90,3 +91,21 @@ const char *new_year_day(double tag, int monat, double jahr, double last_two_dig
     }
     return wochentage[wochentag];
 }
+
+
+int kalenderwoche(double tag, int monat, int year, bool sjahr, double i) {
+    while (new_year_day(i, 1, year, 0.0, 0.0) != "Donnerstag") {
+        i++;
+    }
+
+    tag = tag - (i - 3);
+    i = i - 3;  //Datum des ersten Montags der ersten Woche des Jahres
+    printf("\n%f", ceil(tag / 7.0));
+
+    if(ceil(tag / 7.0) > 52){
+        return 1;
+} else{
+        return ceil(tag / 7.0);
+    }
+}
+
